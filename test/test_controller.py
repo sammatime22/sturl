518b693d76985dcf98aed5b9b73f7e5f59c96026
sammyreles22 <samuel.reles@gmail.com>
2022-12-20 08:34:31 -0500

    test stubs for controller

---
+++ test/test_controller.py
@@ -0,0 +1,35 @@
+# A class to test the controller of STURL.
+# sammatime22, 2022
+import unittest
+
+class TestController(unittest.TestCase):
+    '''
+    A set of tests for the Controller class.
+
+    Parameters
+    ----------
+    unittest.TestCase : from unittest
+        Provides the Unit Test Framework
+    '''
+
+
+    def test_01_place_job_on_queue_successful(self):
+        '''
+        Tests the appropriate response provided a job is placed on the queue.
+        '''
+
+    def test_02_place_job_on_queue_unsuccessful(self):
+        '''
+        Tests the appropriate response provided a job is not placed on the queue.
+        '''
+
+    def test_03_collect_report_successful(self):
+        '''
+        Tests the appropriate response provided a report can be collected.
+        '''
+
+    def test_04_collect_report_unsuccessful(self):
+        '''
+        Tests the appropriate response provided a report cannot be collected.
+        '''
+
