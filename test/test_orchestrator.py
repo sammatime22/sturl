b17980e381c19a521bb65bae86ef3449ef38a466
sammyreles22 <samuel.reles@gmail.com>
2022-12-22 23:13:17 -0500

    adding tests for the orchestrator

---
+++ test/test_orchestrator.py
@@ -0,0 +1,28 @@
+# A class to test the orchestrator of STURL.
+# sammatime22, 2022
+import sys
+sys.path.append('src')
+
+import unittest
+
+class TestOrchestrator(unittest.TestCase):
+    '''
+    A set of tests for the Orchestrator class.
+
+    Parameters:
+    ----------
+    unittest.TestCase : from unittest
+        Provides the Unit Test Framework
+    '''
+
+    def test_01_store_job_request_sent():
+        '''
+        Tests the appropriate message is sent provided a store job is tasked.
+        '''
+        pass
+
+    def test_02_retrieve_job_request_sent():
+        '''
+        Tests the appropriate message is sent provided a retrieve job is tasked.
+        '''
+        pass
